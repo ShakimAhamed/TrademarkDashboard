cc268707067eff1658f124e325b4724460e46899
Shakim Ahamed <sakimahamed7@gmail.com>
2026-04-14 17:55:33 +0300

    Readme update

--- src/components/TrademarkCard.tsx
+++ src/components/TrademarkCard.tsx
@@ -17,20 +17,23 @@ function highlight(text: string, query: string) {
 
 function TrademarkCard({ item, search }: any) {
   return (
-    <div className="p-4 border rounded-xl shadow hover:shadow-lg transition bg-white">
-      <h3 className="font-bold text-lg">
-        {highlight(item.name, search)}
-      </h3>
-      <p className="text-sm text-gray-500">{item.owner}</p>
-      <span
-        className={`inline-block mt-2 px-2 py-1 text-xs rounded ${
-          item.status === "Active"
-            ? "bg-green-100 text-green-700"
-            : "bg-red-100 text-red-700"
-        }`}
-      >
-        {item.status}
-      </span>
+    <div className="border rounded hover:shadow-lg transition">
+
+      <div className="p-4   shadow  bg-white">
+        <h3 className="font-bold text-lg">
+          {highlight(item.name, search)}
+        </h3>
+        <p className="text-sm text-gray-500">{item.owner}</p>
+        <span
+          className={`inline-block mt-2 px-2 py-1 text-xs rounded ${
+            item.status === "Active"
+              ? "bg-green-100 text-green-700"
+              : "bg-red-100 text-red-700"
+          }`}
+        >
+          {item.status}
+        </span>
+      </div>
     </div>
   );
 }
